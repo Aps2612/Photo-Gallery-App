d088318b80fd149a2bcbc282f17dfac077e050f6
Aps2612 <apsingh1722@gmail.com>
2023-03-26 14:37:09 +0530

    photo gallery app

--- server.js
+++ server.js
@@ -8,15 +8,17 @@ app.set('view engine', 'ejs');
 // Serve static files from the public directory
 app.use(express.static('public'));
 
+// Route to retrieve images from Unsplash API and render the index page with images
 app.get('/', async (req, res) => {
   const accessKey = 'oQhIsVTb4qn99UUqkqfsGZr27yoyyxHUpDpNnPobK9w';
-  const url = `https://api.unsplash.com/photos/random?count=12&client_id=${accessKey}`;
+  const url = `https://api.unsplash.com/photos/random?count=16&client_id=${accessKey}`;
 
   try {
     const response = await axios.get(url);
     const images = response.data;
     res.render('index', { images });
   } catch (error) {
+    console.error(error);
     res.send('Error retrieving images');
   }
 });
